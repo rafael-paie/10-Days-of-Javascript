447a33f9d348d2f7a4b9cdb4574dca2841ec874d
rafael-paie <rafaelpaie.dev@gmail.com>
2023-06-07 23:48:48 -0300

    Day 0: Hello World! & Data Types.

---
+++ js10-day0.js
@@ -0,0 +1,20 @@
+//Day 0: Hello World!
+function greeting(inputString) {
+
+    console.log("Hello, World!");
+    console.log(inputString)
+}
+
+//Day 0: Data Types
+function performOperation(secondInteger, secondDecimal, secondString) {
+    
+    const firstInteger = 4;
+    console.log(firstInteger + parseInt(secondInteger));
+    
+    const firstDecimal = 4.0;
+    console.log(firstDecimal + parseFloat(secondDecimal));
+    
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString);
+    
+}
