b6a4ce0db9c68a391bd583f2d3a8a1e68213abfb
rafael-paie <rafaelpaie.dev@gmail.com>
2023-06-07 23:38:53 -0300

    Day 0: Data Types

---
+++ day0.1-js10.js
@@ -0,0 +1,12 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    
+    const firstInteger = 4;
+    console.log(firstInteger + parseInt(secondInteger));
+    
+    const firstDecimal = 4.0;
+    console.log(firstDecimal + parseFloat(secondDecimal));
+    
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString);
+    
+}
